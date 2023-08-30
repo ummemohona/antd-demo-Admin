9703a5286c1afc91fe164eddb7a062788ad696af
ummemohona <ummaymohona488@gmail.com>
2023-08-30 12:07:45 +0600

    Auth Implementaed

--- src/components/logo/index.tsx
+++
@@ -1,8 +0,0 @@
-import Image from "next/image";
-import React from "react";
-
-export const Logo: React.FC = () => {
-  return (
-    <Image src="/icons/nextjs-icon.svg" alt="nextjs" width="96" height="58" />
-  );
-};
